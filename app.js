11dbf0317009694cc201eca8c03263c979393b73
ZahraRahmani <z.zahir2020@gmail.com>
2017-10-23 21:48:33 -0600

    Installing firebase tools

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 (function () {
     var app = angular.module('app', []);
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD-5kNnhWUkxzvMBK9Ci50Yl4EcNYbnvkk",
+    authDomain: "resumebuilder-8c22b.firebaseapp.com",
+    databaseURL: "https://resumebuilder-8c22b.firebaseio.com",
+    projectId: "resumebuilder-8c22b",
+    storageBucket: "resumebuilder-8c22b.appspot.com",
+    messagingSenderId: "823751751382"
+  };
+  firebase.initializeApp(config);
+
+
     app.controller('MyCtrl', MyCtrl);
 
     MyCtrl.$inject = ['$scope','MyService'];
